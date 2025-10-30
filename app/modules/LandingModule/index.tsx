e2e18bdd89cf5db3fd2ced24188acf9ceeb482f7
Reksaditya <reksa@ReksaAditya.localdomain>
2025-10-30 23:03:08 +0700

    About Section

--- app/modules/LandingModule/index.tsx
+++ app/modules/LandingModule/index.tsx
@@ -2,14 +2,15 @@ import { HeroSection } from "./sections/hero"
 import { PreviewSection } from "./sections/preview"
 import { Navbar } from "~/components/ui/navbar"
 import { Loading } from "~/components/ui/loading"
+import { About } from "./sections/about"
 
 export const LandingPage = () => {
   return (
     <div>
-      <Loading />
       <Navbar />
       <HeroSection />
       <PreviewSection />
+      <About />
     </div>
   )
 }
--- app/modules/LandingModule/sections/about.tsx
+++ app/modules/LandingModule/sections/about.tsx
@@ -1,9 +1,20 @@
 export const About = () => {
   return (
-    <section>
-      <h1>About Us</h1>
-      <div>
-        
+    <section className="flex flex-col items-center justify-center gap-10 pt-10 pb-20">
+      <div className="flex items-center justify-center w-full my-8">
+        <div className="grow h-2 bg-primary green-300 max-w-96 rounded-full" />
+        <h2 className="mx-4 text-6xl font-bold italic">About us</h2>
+        <div className="grow h-2 bg-primary max-w-96 rounded-full" />
+      </div>
+      <div className="relative h-150 w-full overflow-hidden">
+        <div className="absolute inset-0 bg-black/50 z-10"></div>
+        <img src="/public/background.webp" alt="background" className="object-cover" />
+        <div className="absolute z-20 inset-0 flex flex-col justify-center px-20 text-white">
+          <h1 className="text-8xl font-extrabold">Sekitar Kita</h1>
+          <p className="max-w-2xl mt-2 text-justify pl-2 text-lg">
+            Sekitar Kita adalah website yang menyediakan direktori UMKM yang ada, khususnya di sekitar Telkom University. Guna membantu mahasiswa tahun pertama yang di wajibkan asrama serta membantu UMKM Naik Kelas dengan menyediakan solusi digital sekaligus layanan informasi mengenai UMKM yang ada di sekitar Telkom University.
+          </p>
+        </div>
       </div>
     </section>
   )
