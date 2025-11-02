e46843a8ed8d397fdac823f97e95997fadee72e2
Reksaditya <reksa@ReksaAditya.localdomain>
2025-11-02 13:19:23 +0700

    Navbar navitagion

--- app/components/ui/navbar.tsx
+++ app/components/ui/navbar.tsx
@@ -1,9 +1,14 @@
 import { useEffect, useState } from "react"
 import { Input } from "./input"
-import { Link } from "react-router"
+import { Link, useLocation, useNavigate } from "react-router"
+import { scroller } from "react-scroll"
 
 export const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false)
+  const navigate = useNavigate()
+  const location = useLocation()
+
+  const isHome = location.pathname === '/'
 
   useEffect(() => {
     const handleScroll = () => {
@@ -15,20 +20,78 @@ export const Navbar = () => {
     }
 
     window.addEventListener('scroll', handleScroll)
-    return() => window.removeEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
   }, [])
 
+  const isLight = !isHome || isScrolled
+
   return (
-    <div className={`flex fixed items-center justify-between w-full px-20 py-3 ${isScrolled ? 'bg-white z-30 shadow-xl' : 'bg-transparent z-20'} transition duration-500`}>
-      <img src={`/public/${isScrolled ? 'logodark.svg' : 'logo.svg'}`} alt="logo" width={150} height={150} className="" />
-      <Input className={`max-w-2xl backdrop-blur-lg ${isScrolled ? 'text-foreground' : 'text-white bg-black/10 border-2 border-white/20'}`} />
-      <nav className={`relative z-20 h-11 w-fit px-5 py-2 rounded-full backdrop-blur-lg ${isScrolled ? 'text-foreground' : 'text-white bg-black/10 border-2 border-white/20'}`}>
+    <div className={`flex items-center justify-between w-full px-20 py-3 transition duration-500 ${isHome
+      ? `fixed ${isScrolled
+        ? "bg-white z-40 shadow-xl"
+        : "bg-transparent z-20"
+      }`
+      : "relative bg-white shadow-md z-30"
+      }`}>
+      <img
+        src={`/public/${isLight ? "logodark.svg" : "logo.svg"}`}
+        alt="logo"
+        width={150}
+        height={150}
+      />
+      <Input
+        className={`max-w-2xl backdrop-blur-lg ${isLight
+          ? "text-foreground"
+          : "text-white bg-black/10 border-2 border-white/20"
+          }`}
+      />
+      <nav
+        className={`relative z-20 h-11 w-fit px-5 py-2 rounded-full backdrop-blur-lg ${isLight
+          ? "text-foreground"
+          : "text-white bg-black/10 border-2 border-white/20"
+          }`}
+      >
         <ul className="list-none flex gap-5">
-          <li className="hover:cursor-pointer">Beranda</li>
-          <Link to={"/product"}>
-            <li className="hover:cursor-pointer">List UMKM</li>
-          </Link>
-          <li className="hover:cursor-pointer">Kontak Kami</li>
+          <li
+            className="hover:cursor-pointer"
+            onClick={() => {
+              if (location.pathname == '/') {
+                scroller.scrollTo('hero', {
+                  smooth: true,
+                  duration: 800,
+                  delay: 0,
+                })
+              } else {
+                navigate('/')
+              }
+            }}
+          >
+            Beranda
+          </li>
+          <li
+            className="hover:cursor-pointer"
+            onClick={() => {
+              navigate('/product')
+            }}
+          >
+            List UMKM
+          </li>
+          <li
+            className="hover:cursor-pointer"
+            onClick={() => {
+              if (location.pathname === '/') {
+                scroller.scrollTo("contact", {
+                  smooth: true,
+                  duration: 800,
+                  delay: 0,
+                })
+              } else {
+                navigate('/')
+              }
+            }}
+          >
+            Kontak Kami
+          </li>
         </ul>
       </nav>
     </div>
