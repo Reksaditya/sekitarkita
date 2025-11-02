51669010bf767a7680dcbb09cf8c8d77a90cb57b
Reksaditya <reksa@ReksaAditya.localdomain>
2025-11-02 14:59:12 +0700

    Product page meta

--- app/routes/product.tsx
+++ app/routes/product.tsx
@@ -1,5 +1,12 @@
 import { ProductPage } from "~/modules/ProductModule"
 
+export function meta() {
+  return [
+    { title: "List UMKM | SekitarKita - Direktori UMKM | Sekitar Telkom University" },
+    { name: "description", content: "Website Direktori UMKM di Sekitar Telkom University" },
+  ];
+}
+
 export default function Product() {
   return <ProductPage />;
 }
