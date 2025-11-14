48e631d7310c723a0c331d7a4ac657a0ea4e0a0a
Reksaditya <reksa@ReksaAditya.localdomain>
2025-11-14 08:39:07 +0700

    Fix stacked ui

--- app/components/ui/carousel.tsx
+++ app/components/ui/carousel.tsx
@@ -136,13 +136,13 @@ function CarouselContent({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       ref={carouselRef}
-      className="overflow-hidden"
+      className="overflow-visible"
       data-slot="carousel-content"
     >
       <div
         className={cn(
           "flex",
-          orientation === "horizontal" ? "-ml-4" : "-mt-4 flex-col",
+          orientation === "horizontal" ? "-ml-32" : "-mt-4 flex-col",
           className
         )}
         {...props}
@@ -161,7 +161,7 @@ function CarouselItem({ className, ...props }: React.ComponentProps<"div">) {
       data-slot="carousel-item"
       className={cn(
         "min-w-0 shrink-0 grow-0 basis-full",
-        orientation === "horizontal" ? "pl-4" : "pt-4",
+        orientation === "horizontal" ? "pl-32" : "pt-4",
         className
       )}
       {...props}
